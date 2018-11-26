b766faa4c9863c955ec32bd58d3472d31b42baad
richardtam.ny <richardtam.ny@gmail.com>
2018-11-26 18:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ function add2(n) {
 var theFunk = funkyFunction()
 
 function funkyFunction() {
-  return "FUNKY!"
+  var theFunk = "FUNKY!"
+  return theFunk
   }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
